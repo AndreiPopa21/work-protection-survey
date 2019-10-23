faa5a1a97ed7d114199b371fcc3f654672407bf8
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-10-23 17:52:58 +0300

    Created MainMenu class for holding all the Menu form event functions

---
+++ work-protection-survey/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work_protection_survey
+{
+    class MainMenu
+    {
+        private static Form menuForm;
+
+        public static void SetForm(Form form)
+        {
+            menuForm = form;
+        }
+        public static void ExitApplication(object sender, EventArgs e)
+        {
+            //Exit application
+            Application.Exit();
+        }
+        public static void MinimizeApplication(object sender, EventArgs e)
+        {
+            //Minimize application
+            menuForm.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
